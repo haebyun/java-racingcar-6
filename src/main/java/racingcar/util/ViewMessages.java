b2874721c876286960eba585759f1049b4e0865b
haebyun <bhk4392@gmail.com>
2024-07-08 20:11:44 +0900

    feat: ViewMessages 클래스 추가 및 출력 메시지 상수 정의
    - 주요 입력 요청 메시지와 실행 결과 출력 메시지를 위한 상수 정의
    - 경주 별 움직임 표시와 우승자 표시를 위한 문자열 상수 추가

---
+++ src/main/java/racingcar/util/ViewMessages.java
@@ -0,0 +1,16 @@
+package racingcar.util;
+
+public class ViewMessages {
+    //입력 요청 메시지
+    public static final String PROMPT_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)";
+    public static final String PROMPT_RACE_COUNT = "시도할 회수는 몇회인가요?";
+
+    //실행 출력 메시지
+    public static final String RESULT_HEADER = "실행 결과";
+    public static final String RESULT_FORMAT = "%s : %s";
+    public static final String RESULT_POSITION_SYMBOL = "-";
+
+    //우승자 출력 메시지
+    public static final String WINNER_MESSAGE = "최종 우승자 : ";
+    public static final String WINNER_SEPARATOR = ", ";
+}
