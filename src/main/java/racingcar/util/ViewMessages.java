5712b0155c85cbb3fbc5bf77afb448183fb8481a
haebyun <bhk4392@gmail.com>
2024-07-08 20:21:02 +0900

    feat: ViewMessages 클래스에 자동차 이름 구분자 상수 추가
    - PROMPT_CAR_NAMES_SEPARATOR 상수를 추가하여 자동차 이름 구분자를 관리

--- src/main/java/racingcar/util/ViewMessages.java
+++ src/main/java/racingcar/util/ViewMessages.java
@@ -4,6 +4,7 @@ public class ViewMessages {
     //입력 요청 메시지
     public static final String PROMPT_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)";
     public static final String PROMPT_RACE_COUNT = "시도할 회수는 몇회인가요?";
+    public static final String PROMPT_CAR_NAMES_SEPARATOR = ",";
 
     //실행 출력 메시지
     public static final String RESULT_HEADER = "실행 결과";
