c679ec2db6cba48e44e51b076a39a9093a44274d
haebyun <bhk4392@gmail.com>
2024-07-08 18:20:41 +0900

    init

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,6 @@
 package racingcar;
 
-import racingcar.games.racing.controller.RacingController;
-
 public class Application {
     public static void main(String[] args) {
-        Game game = new RacingController();
-        game.execute();
     }
 }
--- src/main/java/racingcar/games/racing/util/RacingProcessor.java
+++
@@ -1,8 +0,0 @@
-package racingcar.games.racing.util;
-
-import java.util.List;
-import racingcar.games.racing.domain.Car;
-
-public interface RacingProcessor {
-    void moveProcess(List<Car> cars);
-}
